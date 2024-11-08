7d1b07b580ad1c80d508f156dbff6febb26549ff
Josh Yu <joshyu@foxmail.com>
2024-11-08 09:53:33 +0800

    Chore: bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-multisite-plus-fil",
-    version="1.0.0",
+    version="1.0.1",
     author="Divio AG",
     author_email="info@divio.com",
     url="https://github.com/divio/django-multisite-plus",
